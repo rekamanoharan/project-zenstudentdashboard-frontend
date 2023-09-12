908c5a5b3a36fd5cff220ce5c3ee51f6d22c05d9
rekamanoharan <rekamano15@gmail.com>
2023-09-12 16:30:12 +0530

    added new feauture

--- src/WebcodeName.js
+++ src/WebcodeName.js
@@ -2,27 +2,6 @@ import React from "react";
 import "./Card.css";
 import WebcodeDetails from "./WebcodeDetails";
 
-// import { List } from "antd";
-// const data = [
-//   {
-//     title: "Reka",
-//   },
-// ];
-// const WebcodeName = () => (
-//   <List
-//     itemLayout="horizontal"
-//     dataSource={data}
-//     renderItem={(item, index) => (
-//       <List.Item>
-//         <List.Item.Meta
-//           title={item.title}
-//           description="(B48WD Tamil- First Webcode)
-//           Ice and Fire API"
-//         />
-//       </List.Item>
-//     )}
-//   />
-// );
 const WebcodeName = () => {
   return (
     <div className="card-container" onClick={WebcodeDetails}>
